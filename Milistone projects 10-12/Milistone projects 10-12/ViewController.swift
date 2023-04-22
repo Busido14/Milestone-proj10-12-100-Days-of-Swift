2078f3eb72ab958ba30062a04a7f9e8d1724e78d
Artem <artchzhen@gmail.com>
2023-04-22 18:20:32 +0900

    initial commit devolopment

---
+++ Milistone projects 10-12/Milistone projects 10-12/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  Milistone projects 10-12
+//
+//  Created by Артем Чжен on 22/04/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+      
+        
+    }
+}
+
